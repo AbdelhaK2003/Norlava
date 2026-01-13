6af0d2b030047ea4d9e567227450f70845baaaec
Abdelhak2003 <abdelhakmahfoud2003@gmail.com>
2026-01-13 12:14:45 +0100

    feat: Update to logo.svg for favicon and brand logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -25,7 +25,7 @@ export const Logo = ({ size = "md", showText = true, className = "" }: LogoProps
         {/* Logo Image */}
         <div className="relative w-full h-full flex items-center justify-center overflow-hidden">
           <img
-            src="/Logo1.jpeg"
+            src="/logo.svg"
             alt="Norlava"
             className="w-full h-full object-contain"
           />
